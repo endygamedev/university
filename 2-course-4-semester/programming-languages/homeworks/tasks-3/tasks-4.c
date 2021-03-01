30fd525fdd80b6a76cfc6bf1857c8409c94e04c1
Egor <bronnikov.40@mail.ru>
2021-03-01 18:01:16 +0300

    :student: Edited `tasks-4` in `programming-languages`

--- 2-course-4-semester/programming-languages/homeworks/tasks-3/tasks-4.c
+++ 2-course-4-semester/programming-languages/homeworks/tasks-3/tasks-4.c
@@ -1,35 +1,51 @@
 #include <stdio.h>
 
 
+void print_row(int line, FILE *file)
+{
+	int i = 0, c;
+	while(i < line) {
+		c = fgetc(file);
+		if(c == '\n')
+			i++;
+	}
+	while((c = fgetc(file)) != '\n' && c != EOF)
+		printf("%c", c);
+	printf("\n");
+}
+
+
 int main(int argc, char **argv)
 {
 	FILE *first, *second;
-	int c1, c2;
+	FILE *first_c, *second_c;
+	int c1, c2, line = 0;
 	if(argc < 3) {
 		fprintf(stderr, "Error: too few arguments\n");
 		return 1;
 	}
 	first = fopen(argv[1], "r");
+	first_c = fopen(argv[1], "r");
 	if(!first) {
 		perror(argv[1]);
 		return 2;
 	}
 	second = fopen(argv[2], "r");
+	second_c = fopen(argv[2], "r");
 	if(!second) {
 		perror(argv[2]);
 		return 3;
 	}
-	while((c1 = fgetc(first)) == (c2 = fgetc(second)));
-	printf("%s:\n", argv[1]);
-	while(c1 != '\n' && c1 != EOF) {
-		printf("%c", c1);
-		c1 = fgetc(first);
+	while((c1 = fgetc(first)) == (c2 = fgetc(second)) && c1 != EOF && c2 != EOF) {
+		if(c1 == '\n')
+			line++;
 	}
-	printf("\n%s:\n", argv[2]);
-	while(c2 != '\n' && c2 != EOF) {
-		printf("%c", c2);
-		c2 = fgetc(second);
+	if(c1 == EOF && c2 == EOF) {
+		printf("Files are the same\n");
+	} else {
+		printf("File differ on line %d\n", line + 1);
+		print_row(line, first_c);
+		print_row(line, second_c);
 	}
-	printf("\n");
 	return 0;
 }
