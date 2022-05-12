55bcfb353a2aa031fea463f45fc6473818565796
Egor <bronnikov.40@mail.ru>
2022-05-12 10:35:39 +0300

    Edit `class-8` in `internet-technologies`

--- 3-course-6-semester/internet-technologies/class-8/sample.js
+++ 3-course-6-semester/internet-technologies/class-8/sample.js
@@ -59,7 +59,7 @@ function fromFirstToSecond() {
     for (const circle of circles) {
         const el = createObject(circle.innerHTML);
         el.classList.add("circle");
-		el.style.color = "magenta";
+        el.style.color = "magenta";
         secondContainer.appendChild(el);
         circle.remove();
     }
@@ -72,7 +72,7 @@ function fromSecondToFirst() {
     for (const square of squares) {
         const el = createObject(square.innerHTML);
         el.classList.add("square");
-		el.style.color = "purple";
+        el.style.color = "purple";
         firstContainer.appendChild(el);
         square.remove();
     }
